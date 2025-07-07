3cb3738e094b7865a2f2cfcec90b8e5054313c5b
ChiragRathava29 <rathavachirag29@gmail.com>
2025-07-08 00:19:01 +0530

    merge two sorted lists into one sorted list

---
+++ src/Ex_20.java
@@ -0,0 +1,28 @@
+// How do you merge two sorted lists into one sorted list?
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex_20 {
+
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,3,5,7));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(2,4,6,8));
+
+        List<Integer> Merge_List = mergeSortedLists(list1,list2);
+        System.out.println("Merged Sorted List: " + Merge_List);
+    }
+
+    public static List<Integer> mergeSortedLists(List<Integer> list1,List<Integer> list2){
+        List<Integer> result = new ArrayList<>();
+        int i = 0 , j = 0;
+        while (i < list1.size() && j < list2.size()){
+            if (list1.get(i) < list2.get(j)){ result.add(list1.get(i++)); }
+            else { result.add(list2.get(j++)); }
+        }
+        while (i < list1.size()){ result.add(list1.get(i++)); }
+        while (j < list2.size()){ result.add(list2.get(j++)); }
+        return result;
+    }
+}
