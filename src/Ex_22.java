c7a046a258fc711d9a430015fdbf0c16e91c404e
ChiragRathava29 <rathavachirag29@gmail.com>
2025-07-08 23:26:16 +0530

    iterate through a LinkedList

---
+++ src/Ex_22.java
@@ -0,0 +1,25 @@
+// How do you iterate through a LinkedList?
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Ex_22 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("A","B","C","D"));
+
+        // Using Iterator
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext())
+        { System.out.println(iterator.next()); }
+
+        // Using enhaced for loop
+        for (String item : list)
+        { System.out.println(item); }
+
+        // Using for loop with index
+        for (int i = 0; i < list.size(); i++)
+        { System.out.println(list.get(i)); }
+    }
+}
