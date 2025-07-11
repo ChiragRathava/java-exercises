00188379d026dd71f6e8b95788b2c057db71668d
ChiragRathava29 <rathavachirag29@gmail.com>
2025-07-11 15:40:36 +0530

    Frequency of Each Element in an Array

---
+++ src/Arrays/Ex_32.java
@@ -0,0 +1,20 @@
+// Find the Frequency of Each Element in an Array
+
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex_32 {
+    public static void main(String[] args) {
+
+        int[] numbers = {2,2,3,4,5,5,5,3,2,4};
+        Map<Integer,Integer> frequencyMap = new HashMap<>();
+
+        for (int number : numbers)
+        { frequencyMap.put(number,frequencyMap.getOrDefault(number, 0) + 1); }
+
+        for (Map.Entry<Integer,Integer> entry : frequencyMap.entrySet())
+        { System.out.println("Element: " + entry.getKey() + "Frequency: " + entry.getValue()); }
+    }
+}
