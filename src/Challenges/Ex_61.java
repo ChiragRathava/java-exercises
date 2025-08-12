657c3dde7fb339edf26e7ffe37b946ebaa4df04c
ChiragRathava29 <rathavachirag29@gmail.com>
2025-08-12 23:11:20 +0530

    Merge Two Sorted Lists

---
+++ src/Challenges/Ex_61.java
@@ -0,0 +1,59 @@
+package Challenges;
+
+public class Ex_61 {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode (int val){this.val = val;}
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val)
+            {   current.next = l1;
+                l1 = l1.next;   }
+            else
+            {   current.next = l2;
+                l2 = l2.next;   }
+            current = current.next;
+        }
+
+        if (l1 != null){ current.next = l1;}
+        else { current.next = l2; }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head){
+        while (head != null){
+            System.out.println(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        System.out.print("List 1: ");
+        printList(l1);
+        System.out.print("List 2: ");
+        printList(l2);
+
+        ListNode merged = mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        printList(merged);
+    }
+}
