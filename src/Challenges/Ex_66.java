07fc25103e90497d6ee01822970104ae4fc8ebb1
ChiragRathava29 <rathavachirag29@gmail.com>
2025-09-03 16:03:05 +0530

    FizzBuzz

---
+++ src/Challenges/Ex_66.java
@@ -0,0 +1,35 @@
+package Challenges;
+
+import java.util.Scanner;
+
+public class Ex_66 {
+
+        public static void fizzBuzz(int n) {
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else if (i % 3 == 0)
+                    System.out.println("Fizz");
+                else if (i % 5 == 0)
+                    System.out.println("Buzz");
+                else
+                    System.out.println(i);
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the upper limit (default is 100): ");
+            int n = sc.nextInt();
+
+            if (n < 1) {
+                System.out.println("Range should start from at least 1.");
+            } else {
+                fizzBuzz(n);
+            }
+
+            sc.close();
+        }
+
+}
